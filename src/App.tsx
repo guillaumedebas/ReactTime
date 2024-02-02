73d489fdbe431894534ed3f6d571a1a8675ebdde
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-02-02 17:41:32 +0100

    body bg bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ClockDisplay from './components/ClockDisplay/ClockDiplay';
 import OptionsDialog from './components/OptionsDialog/OptionsDialog'
-
+import { useTheme } from '@mui/material/styles';
 
 const App: React.FC = () => {
+    const theme = useTheme();
+  useEffect(() => {
+    document.body.style.backgroundColor = theme.palette.background.default;
+  }, [theme.palette.background.default]);
+
+
     return (
         <>
             <OptionsDialog />
